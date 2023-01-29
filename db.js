0369df580592329463b58b626f36823404178052
Drew Anello <drew.anello@gmail.com>
2023-01-28 20:35:10 -0500

    creates db.js file adds info

---
+++ db.js
@@ -0,0 +1,15 @@
+const Pool = require('pg').Pool
+require('dotenv').config()
+const dbPort = process.env.DB_PORT
+const dbUser = process.env.DB_USER
+const dbHost = process.env.DB_HOST
+const db = process.env.DB
+const dbPW = process.env.DB_PW
+const pool = new pool({
+  user: dbUser,
+  host: dbHost,
+  database: db,
+  password: dbPWm,
+  port: dbPort
+})
+module.exports = pool
--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
-
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 app.listen(PORT, () => console.log('I was in the Pool! I was in the Pool!'))
