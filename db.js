f165d7e1fbfd9733bd5e7bd824cd54abb6926380
Drew Anello <drew.anello@gmail.com>
2023-02-14 15:49:12 -0500

    wip

--- db.js
+++ db.js
@@ -5,7 +5,19 @@ const dbUser = process.env.DB_USER
 const dbHost = process.env.DB_HOST
 const db = process.env.DB
 const dbPW = process.env.DB_PW
+const DATABSE_URL = process.env.DATABSE_URL
+const PGDATABASE = process.env.PGDATABASE
+const PGHOST = process.env.PGHOST
+const PGPASSWORD = process.env.PGPASSWORD
+const PGPORT = process.env.PGPORT
+const PGUSER = process.env.PGUSER
+
 const pool = new Pool({
+  // user: PGUSER,
+  // host: PGHOST,
+  // database: DATABSE_URL,
+  // password: PGPASSWORD,
+  // port: PGPORT
   user: dbUser,
   host: dbHost,
   database: db,
